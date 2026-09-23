872722b31785002d542dc5ccd8a2283fbb2ab3a8
Angular <haseeb_dc@yahoo.com>
2026-09-23 23:21:41 +0200

    Update variables.tf

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -15,3 +15,30 @@ variable "github_branch" {
   type    = string
   default = "main"
 }
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "Target deployment environment"
+}
+
+variable "servicenow_secret_arn" {
+  type        = string
+  description = "AWS Secrets Manager ARN storing ServiceNow credentials (URL, username, password)"
+}
+
+variable "github_org" {
+  type        = string
+  description = "GitHub Organization or username"
+}
+
+variable "github_repo" {
+  type        = string
+  description = "GitHub repository name"
+}
+
+variable "github_branch" {
+  type        = string
+  default     = "main"
+  description = "Deployment branch name"
+}
