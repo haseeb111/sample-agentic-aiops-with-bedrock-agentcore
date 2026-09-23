f767ae5306919ca86bc9e946c08ee2ce732f0e7f
Angular <haseeb_dc@yahoo.com>
2026-09-23 23:24:58 +0200

    Update variables.tf

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -4,6 +4,11 @@ variable "environment" {
   description = "Target deployment environment"
 }
 
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
 variable "servicenow_secret_arn" {
   type        = string
   description = "AWS Secrets Manager ARN storing ServiceNow credentials (URL, username, password)"
