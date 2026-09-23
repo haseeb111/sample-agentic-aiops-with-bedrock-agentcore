5d1cef918051f8a004f9bddb5a7c5130b8bce1f9
Angular <haseeb_dc@yahoo.com>
2026-09-23 23:22:42 +0200

    Update variables.tf

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -1,21 +1,3 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "github_org" {
-  type = string
-}
-
-variable "github_repo" {
-  type = string
-}
-
-variable "github_branch" {
-  type    = string
-  default = "main"
-}
-
 variable "environment" {
   type        = string
   default     = "dev"
